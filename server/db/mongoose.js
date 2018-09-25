f719aa8cc02425860a99ce1c7923e934eaf1483b
Shailesh Sakhare <shailesh.sakhare.contractor@move.com>
2018-09-26 02:07:00 +0530

    heroku commit connection changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://controllo.local:27017/TodoApp', { useNewUrlParser: true });
+//|| 'mongodb://controllo.local:27017/TodoApp
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 module.exports = { mongoose };
